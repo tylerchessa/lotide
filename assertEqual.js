df9df64ca34975e046c99541f93eec0d4d5802c3
tylerchessa <tylerchessa@gmail.com>
2023-02-06 23:21:23 +0000

    assertEqual with emoji

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("❌Assertion Failed " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, "one");
