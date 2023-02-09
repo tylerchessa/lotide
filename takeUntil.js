5215376fd5fc2b72176b3f7d1439835b5759c7c6
tylerchessa <tylerchessa@gmail.com>
2023-02-09 23:54:10 +0000

    changes, assertion added

--- takeUntil.js
+++ takeUntil.js
@@ -22,4 +22,26 @@ const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 
+// compares 2 arrays 
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  //print first array into string
+  for (let x = 0; x < firstArray.length; x++) {
+    if (firstArray[x] !== secondArray[x]) {
+      return false;
+    }
+  } return true;
+};
+ 
+//compares 2 arrays and returns true if they are the exact same
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray) === true) {
+    return (`✅Assertion Passed: True`);
+  } else if (eqArrays(firstArray, secondArray) === false) {
+    return (`❌Assertion Failed: False`);
+  }
+};
 
+console.log(assertArraysEqual((results1),[ 1, 2, 5, 7, 2 ]));
