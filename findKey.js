5461ce6e5988a17d6c3077162c81ad1c71ed9162
tylerchessa <tylerchessa@gmail.com>
2023-02-10 00:17:50 +0000

    created function

---
+++ findKey.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${actual} !==  ${expected}`);
+  }
+
+};
+
+const findkey = (object, callback) => {
+  // loop through keys of object
+  for (let key in object) {
+    // if key of object = callback return key 
+    if (callback(object[key])) {
+      return key
+    }
+  } 
+};
+
+
+findkey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(findkey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+console.log(assertEqual(findkey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"))
