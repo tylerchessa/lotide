0afac72c69a92e17a6e306e3f9bb72f0c050e4db
tylerchessa <tylerchessa@gmail.com>
2023-02-14 01:28:29 +0000

    added middleTest.js with chai and tailTest.js with chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,19 @@
 const middle = require('../middle')
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert
 
-const middleTest1 = middle([1, 2, 3, 4])
-const middleTest2 = middle([1, 2, 3, 4, 5])
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+  });
+});
 
 
-console.log(assertArraysEqual(middleTest1, [2, 3]));
-console.log(assertArraysEqual(middleTest2, [3]));
+// const middleTest1 = middle([1, 2, 3, 4])
+// const middleTest2 = middle([1, 2, 3, 4, 5])
+
+
+// console.log(assertArraysEqual(middleTest1, [2, 3]));
+// console.log(assertArraysEqual(middleTest2, [3]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert
+const tail = require('../tail')
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
+
