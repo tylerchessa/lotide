a722e89964eca8643aefad7e4244031a839a3645
tylerchessa <tylerchessa@gmail.com>
2023-02-14 02:01:23 +0000

    created a file for all functions index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const assertObjects = require('./assertObjects')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays')
+const eqObjects = require('./eqObjects')
+const findKey = require('./findKey')
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
+const letterPositions = require('./letterPositions')
+const loopyLighthouseRefactor = require('./loopyLighthouseRefactor')
+const map = require('./map')
+const pigLatin = require('./pig-latin')
+const reverse = require('./reverse')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjects: assertObjects,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  loopyLighthouseRefactor: loopyLighthouseRefactor,
+  map: map,
+  pigLatin: pigLatin,
+  reverse: reverse,
+  takeUntil: takeUntil,
+  without: without
+
+};
